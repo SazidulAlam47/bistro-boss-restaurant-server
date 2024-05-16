17917771ce6f6dd55683f2d0df2dc48485741648
Sazidul Alam <sazidulalam20@gmail.com>
2024-05-16 23:13:57 +0600

    dashboard

--- index.js
+++ index.js
@@ -27,15 +27,105 @@ async function run() {
 
         const database = client.db("BistroDB");
         const menusCollection = database.collection("menu");
+        const cartsCollection = database.collection("cart");
+        const userCollection = database.collection("users");
 
         app.get("/menus", async (req, res) => {
             let filter = {};
-            if (req.query?.category) {
-                filter = { category: req.query.category }
+            const page = parseInt(req.query?.page);
+            const size = parseInt(req.query?.size);
+            const query = req.query?.category;
+            console.log({ page, size });
+            if (query) {
+                filter = { category: query }
             }
-            const result = await menusCollection.find(filter).toArray();
+            const result = await menusCollection.find(filter).skip(page * size).limit(size).toArray();
+            res.send(result);
+
+        });
+
+        app.get("/menusCount", async (req, res) => {
+            let options = {};
+            const query = req.query?.category;
+            console.log(query);
+            if (query) {
+                options = { category: query }
+            }
+            const count = await menusCollection.countDocuments(options);
+            res.send({ count });
+
+        });
+
+        app.post("/carts", async (req, res) => {
+            const cart = req.body;
+            console.log(cart);
+            const result = await cartsCollection.insertOne(cart);
             res.send(result);
         });
+
+        app.get("/carts", async (req, res) => {
+            let options = {};
+            const email = req.query?.email;
+            if (email) {
+                options = { userEmail: email };
+            }
+            const result = await cartsCollection.find(options).toArray();
+            res.send(result);
+        });
+
+        app.delete("/carts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.delete("/carts/", async (req, res) => {
+            const email = req.query?.email;
+            const query = { userEmail: email };
+            const result = await cartsCollection.deleteMany(query);
+            res.send(result);
+        });
+
+
+        app.get("/users", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.put("/users", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const UpdatedUser = {
+                $set: {
+                    name: user.name,
+                    email: user.email,
+                    image: user.image,
+                }
+            };
+            const result = await userCollection.updateOne(filter, UpdatedUser, options);
+            res.send(result);
+        });
+
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            console.log(id, user);
+            const filter = { _id: new ObjectId(id) };
+            const UpdatedUser = {
+                $set: {
+                    role: user.role,
+                }
+            };
+            const result = await userCollection.updateOne(filter, UpdatedUser);
+            res.send(result);
+        });
+
+
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
